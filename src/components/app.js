af9d12219a6d438d4f9ff88f95255be52de413f8
saraogiraj94 <saraogi.raj40@gmail.com>
2018-03-27 15:43:16 +0530

    added container and new componets for the chart product

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import BooksComponent from './books';
 import BookDetailComponent from './book_detail';
 
+import SearchBarComponent from '../containers/searchbar';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <BooksComponent />
-        <BookDetailComponent />
+        < SearchBarComponent />
       </div>
     );
   }
