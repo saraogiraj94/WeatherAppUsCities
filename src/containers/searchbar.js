ae2e47faf5fbb2216ace3613371fb50a1175e2cb
saraogiraj94 <saraogi.raj40@gmail.com>
2018-03-27 16:35:39 +0530

    added the change listener to search input

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -1,11 +1,36 @@
 import React,{Component} from 'react';
 
 export default class SearchBarComponent extends Component{
+
+    constructor(props){
+        super(props);
+
+        // Intializing the state with an empty string
+        this.state = {term: ' '};
+
+        //bind is done to handle use of this in callbacks
+        this.onInputChange=this.onInputChange.bind(this);
+    }
+
     render(){
         return(
             <form className="input-group">
-                <input />
+                <input
+                    placeholder="Get Five Day Forecast " 
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}/>
+                <span className="input-group-btn">
+                    <button className="btn btn-secondary" type="submit">Submit</button>
+                </span>
             </form>
-        )
+        )   
+    }
+
+    onInputChange(event){
+        console.log("The input is "+event.target.value);
+        this.setState({
+            term:event.target.value
+        })
     }
 }
