8be1900d8632e650dcf36458a1ed98a40f24b12f
saraogiraj94 <saraogi.raj40@gmail.com>
2018-03-28 12:24:53 +0530

    added the average and almost completed the table

--- src/containers/chart.js
+++ src/containers/chart.js
@@ -1,13 +1,22 @@
 // We can make use of function based component
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLines } from 'react-sparklines';
 import React from 'react';
+import _ from 'lodash';
 
 export default (props) => {
     return (
         <div>
             <Sparklines height={120} width={180} data={props.data}>
                 <SparklinesLine color={props.color} />
+                
             </Sparklines>
+            <div>
+                {average(props.data)}
+            </div>
         </div>
     )   
+}
+
+function average(data){
+    return _.round(_.sum(data)/data.length);
 }
